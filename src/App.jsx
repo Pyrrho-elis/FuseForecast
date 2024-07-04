6ccea5687798a29334f0b658bbfa9f7db1aaddac
Pyrrho <67134966+Pyrrho-elis@users.noreply.github.com>
2024-07-04 13:25:53 -0700

    test ad

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <Nav />
+      <div id='frame' style='width:970px;'><iframe data-aa='2336860' src='//ad.a-ads.com/2336860?size=970x250' style='width:970px; height:250px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe><a style='display: block; text-align: right; font-size: 12px' id='preview-link' href='https://a-ads.com/?partner=2336860'>Advertise here</a></div>
       <div className="flex-col justify-center align-center mx-auto my-3 gap-4 max-w-[600px] bg-base-200">
         <Hero />
         <Graph />
